8f497f45f428b1b1a47fb1979c6f2e75957ead3d
volp <tvolp@BNE-28-2057-W.sae.au>
2015-12-11 12:20:41 +1000

    Added timed shake on bell interaction

--- shakecomponent.cpp
+++ shakecomponent.cpp
@@ -1,4 +1,5 @@
 #include "shakecomponent.h"
+#include "apptime.h"
 #include "entity.h"
 #include "player.h"
 #include "app.h"
@@ -13,7 +14,21 @@ void ShakeComponent::OnInit() {
 }
 
 void ShakeComponent::OnUpdate() {
+	if (timerSeconds > AppTime::appTime) {
+		player->ShakeCamera(fadeShakeAmount * (f32)fmin(timerSeconds - AppTime::appTime, fadeSeconds));
+	}
+
+	// This should override timer
 	if (isShaking) {
 		player->ShakeCamera(0.02f);
+	}
+}
+
+// Shake amount, time to shake in seconds exclusive of decay, optional time to decay in seconds
+void ShakeComponent::SetTimer(f32 shakeAmount, f64 time, f64 fadeTime) {
+	if (AppTime::appTime + time > timerSeconds) {
+		timerSeconds = AppTime::appTime + time + fadeTime;
+		fadeSeconds = fadeTime;
+		fadeShakeAmount = shakeAmount;
 	}
 }
